46f13f82f2476d6fc545bbccac3672722d4d0da0
fdgongora <fdgongora@iesmurgi.org>
2021-10-15 10:13:15 +0200

    Arreglar nombres

--- src/main/java/com/example/calculadora/HelloController.java
+++ src/main/java/com/example/calculadora/HelloController.java
@@ -14,10 +14,9 @@ import javafx.scene.text.Text;
 public class HelloController {
 
     @FXML
-    private GridPane gpcalculadora;
+    private GridPane gpCalculadora;
     @FXML
     private TextField tfVisor;
-
 
 
     @FXML
@@ -26,27 +25,30 @@ public class HelloController {
 
     }
 
-    public void initialize () {
+    public void initialize() {
         System.out.println("Inicializando");
         tfVisor.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-        gpcalculadora.addEventFilter(MouseEvent.MOUSE_CLICKED,
+        gpCalculadora.addEventFilter(MouseEvent.MOUSE_CLICKED,
                 event -> {
-                        System.out.println(event.toString());
-                        // Si es un click en un botón o en el texto que tiene el botón (OJO que tiene otro target)
-                        // el source no se puede usar pues toma el grid
-                         // if ( event.getTarget() instanceof Button || event.getTarget() instanceof Text) {
-                    if ( event.getTarget() instanceof Button || ((event.getTarget() instanceof Text) && ((Text) event.getTarget()).getParent() instanceof Button)) {
+                    System.out.println(event.toString());
+                    // Si es un click en un botón o en el texto que tiene el botón (OJO que tiene otro target)
+                    // el source no se puede usar pues toma el grid
+                    // if ( event.getTarget() instanceof Button || event.getTarget() instanceof Text) {
+                    if (event.getTarget() instanceof Button ||
+                            ((event.getTarget() instanceof Text) && ((Text) event.getTarget()).getParent() instanceof Button)) {
 
-                    String texto = (event.getTarget() instanceof Button)? ((Button) event.getTarget()).getText() : ((Text) event.getTarget()).getText();
+                        String texto = (event.getTarget() instanceof Button) ? ((Button) event.getTarget()).getText() : ((Text) event.getTarget()).getText();
 
-                            if (!(texto.equals("+") || texto.equals("-")
-                                    || texto.equals("*") || texto.equals("/")
-                                    || texto.equals("%") || texto.equals(".")
-                                    || texto.equals("="))) {
-                                tfVisor.setText(tfVisor.getText().equals("0") ? "" : tfVisor.getText() + texto);
-                            }
-                            event.consume();
+                        if (!(texto.equals("+") || texto.equals("-")
+                                || texto.equals("*") || texto.equals("/")
+                                || texto.equals("%") || texto.equals(".")
+                                || texto.equals("="))) {
+                            tfVisor.setText(tfVisor.getText().equals("0") ? "" : tfVisor.getText() + texto);
                         }
+                        event.consume();
+                    }
                 });
     }
+
+
 }
